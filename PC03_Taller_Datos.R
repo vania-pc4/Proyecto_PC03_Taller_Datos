de5290677355594b60a0f62565d7f8ce0b5a3201
vania-pc4 <vania.pradoc@pucp.edu.pe>
2026-06-19 13:29:41 -0500

    feat: setup inicial, estructura de carpetas y renv

--- PC03_Taller_Datos.R
+++ PC03_Taller_Datos.R
@@ -21,3 +21,24 @@ install.packages("rlang")
 install.packages("gitcreds")  # Solo la primera vez
 
 # Inicializar Git en el proyecto
+usethis::use_git()
+
+# Paso 3a: Crear un token en GitHub
+usethis::create_github_token()
+# → Esto abrirá tu navegador en GitHub
+# → Ponle un nombre al token (ej: "PUCP_R")
+# → Selecciona scopes: repo, workflow, gist (ya vienen marcados)
+# → Clic en "Generate token"
+# → COPIA el token (empieza con ghp_...)
+
+# Paso 3b: Guardar el token en tu computadora
+gitcreds::gitcreds_set()
+# → Pega el token cuando te lo pida
+
+# Paso 3c: Crear el repositorio público en GitHub
+usethis::use_github(private = FALSE)
+
+# Paso 4: .gitignore: excluir datos crudos pesados de ENAHO
+usethis::edit_git_ignore()
+install.packages("renv")
+renv::init()
